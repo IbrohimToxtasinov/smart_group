18798d21a51e5d00fa1bbc9dee2f7c6684fc94c0
IbrohimToxtasinov <toxtasinovibrohim976@gmail.com>
2023-01-29 08:47:08 +0500

    some changes

---
+++ lib/utils/date_format.dart
@@ -0,0 +1,17 @@
+import 'package:intl/intl.dart';
+
+class TimeUtils {
+  static String formatToMyTime(DateTime dateTime) {
+    final format = DateFormat.d().format(dateTime);
+    return format;
+  }
+
+  static String formatToWeek(DateTime dateTime) {
+    final format = DateFormat.E().format(dateTime);
+    return format;
+  }
+  static String birthDate(DateTime dateTime) {
+    final format = DateFormat.yMMMMd().format(dateTime).toString();
+    return format;
+  }
+}
---
+++ lib/utils/style.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class MyTextStyle {
+  static const TextStyle sfProLight = TextStyle(
+    fontWeight: FontWeight.w300,
+    fontFamily: 'SourceSansPro',
+    fontSize: 14,
+  );
+
+  static const TextStyle sfProRegular = TextStyle(
+    fontWeight: FontWeight.w400,
+    fontFamily: 'SourceSansPro',
+    fontSize: 14,
+  );
+
+  static const TextStyle sfProMedium = TextStyle(
+    fontWeight: FontWeight.w500,
+    fontFamily: 'SourceSansPro',
+    fontSize: 14,
+  );
+
+  static const TextStyle sfProSemiBold = TextStyle(
+    fontFamily: 'SourceSansPro',
+    fontWeight: FontWeight.w600,
+    fontSize: 14,
+  );
+
+  static const TextStyle sfProBold = TextStyle(
+    fontWeight: FontWeight.w700,
+    fontFamily: 'SourceSansPro',
+    fontSize: 14,
+  );
+
+  static const TextStyle sfBold800 = TextStyle(
+    fontWeight: FontWeight.w800,
+    fontFamily: 'SourceSansPro',
+    fontSize: 14,
+  );
+
+  static const TextStyle sfProBlack = TextStyle(
+    fontWeight: FontWeight.w900,
+    fontFamily: 'SourceSansPro',
+    fontSize: 14,
+  );
+}
