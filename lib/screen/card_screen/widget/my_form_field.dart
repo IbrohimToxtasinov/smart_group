e057ed97e7ba288a7fa43d121117b742f9753f29
Muhammadyunusxon <muhammadyunusxonabdumannopov9@gmail.com>
2023-01-29 19:59:40 +0500

    date fixed

--- lib/screen/card_screen/widget/my_form_field.dart
+++ lib/screen/card_screen/widget/my_form_field.dart
@@ -57,16 +57,14 @@ class _MyFormFieldState extends State<MyFormField> {
                   if (DateTime.now().year.toString() == "20${s.substring(3)}") {
                     if (DateTime.now().month >= int.parse(s.substring(0, 2))) {
                       setState(() => isError = true);
-                      print('"er5r');
                     }
-                  }
-                  else if (DateTime.now().year > int.parse("20${s.substring(3)}")) {
+                  } else if (DateTime.now().year >
+                      int.parse("20${s.substring(3)}")) {
                     setState(() => isError = true);
-                  }else if(int.parse(s.substring(0, 2))>12){
+                  } else if (int.parse(s.substring(0, 2)) > 12) {
                     setState(() => isError = true);
                   }
-                }
-                else{
+                } else {
                   setState(() => isError = false);
                 }
               } else if (widget.title == "Karta nomi") {
